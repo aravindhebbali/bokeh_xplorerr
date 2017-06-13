0f5b2bf68edb35f11dccddfda3a77700cf9d3120
rsquaredin <rsquaredin@gmail.com>
2017-06-13 22:09:32 +0530

    finalize arguments for univariate bar plot

--- bobar.R
+++ bobar.R
@@ -2,12 +2,7 @@
 ba <- figure() %>%
   ly_bar(x = levels(cyl), y = as.vector(table(cyl)), 
          data = mtcars, hover = TRUE, alpha = 0.8, width = 0.7,
-         fill_color = 'red')
+         fill_color = 'red', fill_alpha = 1, line_color = 'red',
+         line_alpha = 1)
 ba
 
-# stacked/grouped bar plot
-ba <- figure() %>%
-  ly_bar(x = cyl, y = rep(1, length(cyl)), position = 'dodge',
-         data = mtcars, color = gear, hover = TRUE, alpha = 0.8,
-         width = 0.7)
-ba
